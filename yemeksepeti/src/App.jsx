21b7faa7d91d603420717660087fadd36c8878ca
Osman Bay <bayosman342@gmail.com>
2025-03-05 14:31:35 +0300

    Home page created

--- yemeksepeti/src/App.jsx
+++ yemeksepeti/src/App.jsx
@@ -1,14 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
-import "./App.css"
+import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ yemeksepeti/src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import homePicture from "../../assets/mainpicture.webp";
+import "./Home.css";
+import { FaLocationCrosshairs } from "react-icons/fa6";
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  return (
+    <div className="home-container">
+      <div className="home-picture-div">
+        <img src={homePicture} alt="" className="home-image" />
+      </div>
+      <div className="home-info">
+        <h2>Yemek ya da market, tüm ihtiyaçların kapında!</h2>
+        <div className="input-area">
+          <div className="parent-input">
+            <div className="input-container">
+              <input
+                type="text"
+                placeholder="Örneğin: Esentepe Dede Korkut Sk. No:28/1"
+                id="floating-input"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <label htmlFor="floating-input">Adresini Belirle veya Seç</label>
+              <button className="home-btn">Keşfet</button>
+            </div>
+            <div className="location">
+              <FaLocationCrosshairs className="icon" />{" "}
+              <span>Konumumu Bul</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
