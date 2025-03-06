f3ba7e4b601f2c88d29359184616b7f9a2812cba
Osman Bay <bayosman342@gmail.com>
2025-03-06 17:23:01 +0300

    Article page created and made responsive

--- yemeksepeti/src/App.jsx
+++ yemeksepeti/src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Article from "./pages/Article/Article";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Article />
     </>
   );
 }
---
+++ yemeksepeti/src/pages/Article/Article.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Article.css";
+import articleImage from "../../assets/article-image.webp";
+
+export default function Article() {
+  return (
+    
+      <div className="article-container">
+        <h2>Restoran veya Mahalle esnafı iş ortağımız olun</h2>
+        <div className="relative-image">
+          <img src={articleImage} alt="" className="article-img" />
+          <div className="article-absolute">
+            <h3>
+              Hemen katılın, Yemeksepeti ile satışlarınızı katlamaya başlayın
+            </h3>
+            <p>
+              Binlerce yeni kullanıcıya ulaşabilmek ister misiniz? Sadece kendi
+              semtinizdekiler değil, artık çevre semtlerdeki binlerce kullanıcı
+              da işletmenizden sipariş verebilecek. Bir ev, bir plaza, bir
+              işyeri ya da üniversite kampüsü, hizmet vermek istediğiniz kim
+              varsa artık müşteriniz.
+            </p>
+            <p>
+              Gayet basit! Menünüzü listeliyoruz ve sipariş bilgilerini size
+              ulaştırıyoruz. Sipariş teslimatını dilerseniz kendiniz
+              yapabiliyorsunuz, dilerseniz Yemeksepeti sizin adınıza kullanıcıya
+              ulaştırıyor.
+            </p>
+            <p>
+              Hemen ailemize katılabilir ve muhteşem avantajlara sahip iş
+              ortaklarımızdan biri olabilirsiniz.
+            </p>
+            <button className="article-btn">Başlayalım</button>
+          </div>
+        </div>
+      </div>
+    
+  );
+}
